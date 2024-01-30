5299a90ada4aea763ad0555cb17c76292557f7d4
Ed-Visinheski <eduardo.cook-visinheski@city.ac.uk>
2024-01-30 19:05:29 +0000

    Added TokenType

--- Lox.java
+++ Lox.java
@@ -8,6 +8,7 @@ import java.nio.files.Paths;
 import java.util.List;
 
 public class Lox {
+    static boolean hadError = false;
     public static void main(String[] args) throws IOException{
         if (args.length > 1){
             System.out.println("Usage: jlox [script]");
@@ -17,5 +18,39 @@ public class Lox {
         } else{
             runPrompt();
         }
+    }
+    private static void runFile(String path) throws IOException{
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+
+        if(hadError) System.exit(65);
+    }
+    private static voide runPrompt() throws IOException{
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for(;;){
+            System.out.print("> ");
+            String line = reader.readLine();
+            if (line == null) break;
+            run(line);
+            hadError = false;
+        }
+    }
+    private static void run (String source){
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        for(Token token : tokens){
+            System.out.println(token);
+        }
+    }
+
+    static voide error(int line, String message){
+        report(line, "", message);
+    }
+    private static voide report(int line, String where, String message){
+        System.err.println("[line " + line + "] Error'" + where + ": " + message);
+        hadError = true;
     }
 }
